110536e2eba7e86f416fbc1bcee1231611959d13
Grzegorz Wolski <gwolski93@gmail.com>
2020-01-19 12:02:13 +0100

    Fixed InvoiceTest, Collection into Map

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,22 +2,30 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
-
+	private Map<Product, Integer> products = new HashMap<>();
+	
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.addProduct(product, 1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		this.products.put(product, quantity);
 	}
 
 	public BigDecimal getNetPrice() {
-		return new BigDecimal(0);
+		
+		BigDecimal sum = new BigDecimal(0);
+		for (Product product : this.products.keySet()) {
+			Integer quantity = this.products.get(product);
+		}
+		
+		return sum;
 	}
 
 	public BigDecimal getTax() {
