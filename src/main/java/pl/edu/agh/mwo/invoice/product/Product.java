958640dfca266d04be52098e02f224659a7aaaca
Grzegorz Wolski <gwolski93@gmail.com>
2020-01-19 11:17:58 +0100

    Complete fixed ProductTest

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -13,6 +13,11 @@ public abstract class Product {
 		if ( name == null || name.isEmpty() ) {
 			throw new IllegalArgumentException("Product name cannot be null");
 		}
+		
+		if ( price == null || price.compareTo(new BigDecimal(0)) < 0 ) {
+			throw new IllegalArgumentException("Product price cannot be null");
+		}
+		
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
