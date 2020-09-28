acc3b1f93c5f75a46979d0bb8df1928f03f884ce
Grzegorz Wolski <gwolski93@gmail.com>
2020-09-28 23:00:16 +0200

    Adding excise, what a fight - O_O soo many changes

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -4,25 +4,32 @@ import java.math.BigDecimal;
 
 public abstract class Product {
     private final String name;
-
     private final BigDecimal price;
-
     private final BigDecimal taxPercent;
+    private final BigDecimal excise;
 
-    protected Product(String name, BigDecimal price, BigDecimal tax) {
+    protected Product(String name, BigDecimal price, BigDecimal tax, BigDecimal excise) {
         if (name == null
             || name.equals("")
             || price == null
             || tax == null
+            || excise == null
             || tax.compareTo(new BigDecimal(0)) < 0
-            || price.compareTo(new BigDecimal(0)) < 0) {
+            || price.compareTo(new BigDecimal(0)) < 0
+            || excise.compareTo(new BigDecimal(0)) < 0) {
             throw new IllegalArgumentException();
         }
+        
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
+        this.excise = excise;
     }
 
+    protected Product(String name, BigDecimal price, BigDecimal tax) {
+        this(name, price, tax, BigDecimal.ZERO);
+    }
+    
     public String getName() {
         return name;
     }
@@ -35,7 +42,12 @@ public abstract class Product {
         return taxPercent;
     }
 
-    public BigDecimal getPriceWithTax() {
-        return price.multiply(taxPercent).add(price);
+    public BigDecimal getExcise() {
+        return excise;
     }
+
+    public BigDecimal getPriceWithTax() {
+        return price.multiply(taxPercent).add(price).add(excise);
+    }
+    
 }
