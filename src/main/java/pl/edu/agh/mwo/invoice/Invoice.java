5427f545272a1a8afd4a6f8a299bff963c00d515
Grzegorz Wolski <gwolski93@gmail.com>
2020-09-28 19:10:17 +0200

    adding number to invoice

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -7,7 +7,9 @@ import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
     private Map<Product, Integer> products = new HashMap<Product, Integer>();
-
+    private static int nextNumber = 0;
+    private final int number = ++nextNumber;
+    
     public void addProduct(Product product) {
         addProduct(product, 1);
     }
@@ -38,5 +40,9 @@ public class Invoice {
             totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
         }
         return totalGross;
+    }
+    
+    public int getNumber() {
+        return number;
     }
 }
